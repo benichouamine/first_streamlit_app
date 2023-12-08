0d86a9d97e3eafe203c88b722d35eb6c6a7fa708
benichouamine <144104748+benichouamine@users.noreply.github.com>
2023-12-08 12:27:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parent New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebarry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Buled Free-Range Egg')
+
